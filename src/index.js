7c246729c11f5921a6db5e94112de9af449de36e
SujalDeep <deepsujal2000@gmail.com>
2022-08-18 20:32:25 +0530

    adding mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
 const app = express();
+
+// app.use(express.bodyParser());
+app.use(express.json());
 const staticpath = path.join(__dirname , "../public");
 app.use(express.static(staticpath));
 app.get("/" , (req , resp) => {
     resp.send("Hello...");
 });
+
+const connnetMongo = async () => mongoose.connect('mongodb+srv://ayushajay:ayushajay@cluster0.whbaums.mongodb.net/?retryWrites=true&w=majority')
+const emailSchema = new mongoose.Schema({
+  email: {
+    type:String,
+    required:true, 
+    unique:true,
+  },
+  name : {
+    type:String,
+  },
+  last : {
+    type:String,
+  }
+});
+const emailModel = mongoose.models.Email || mongoose.model("Email" , emailSchema);
+app.post("/send" , async (req,res)=>{
+    await connnetMongo();
+    const email = await emailModel.create(req.body);
+    console.log(req.body);
+    res.status(200).send('done');
+})
+
 app.listen(process.env.PORT || 8000 , () => {
-    console.log(`listening..`);
+    console.log(`listening on http://localhost:8000`);
 });
